e65e270656242075f0b06d304e0b8c53a25f9481
mohidqadeer123 <moqadeer@iu.edu>
2025-10-08 01:51:15 +0000

    Ex 1

--- apputil.py
+++ apputil.py
@@ -1,1 +1,14 @@
-# your code here ...
+import requests
+import pandas as pd
+
+# Exercise 1
+# Create class 'Genius'
+class Genius:
+    def __init__(self, access_token)
+    if not access_token:
+        raise ValueError("Access token is required to access Genius API")
+        self.acesstoken = access_token
+
+
+# Exercise 2
+
