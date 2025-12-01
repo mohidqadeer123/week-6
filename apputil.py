4d37fa0a9860aa6e9d2f53f8c786ac46efb9fc26
mohidqadeer123 <moqadeer@iu.edu>
2025-12-01 00:27:25 -0500

    Update apputil.py

--- apputil.py
+++ apputil.py
@@ -41,7 +41,7 @@ class Genius:
         rows = []
 
         for term in search_terms:
-            artist = self.get_artist(term)
+            artist_data = self.get_artist(term)
             if isinstance(artist_data, dict) and 'response' in artist_data:
                 artist = artist_data['response'].get('artist', {})
             else:
